743236ec06e7ac07773984f8f19b1c561c096682
Rakshith <harshith.shetty2002@gmail.com>
2025-11-06 15:10:20 +0530

    Update solutionOne.js

--- frontend/solutionOne.js
+++ frontend/solutionOne.js
@@ -91,3 +91,108 @@ var removeNthFromEnd = function(head, n) {
    return dummy.next;
 
 };
+
+/* Problem : 16 
+    Valid Parentheses
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+
+Example 5:
+
+Input: s = "([)]"
+
+Output: false
+*/
+
+//   Answer 
+
+var isValid = function(s) {
+    const stack = [];
+    const data = {
+        '(' : ')',
+        '{' : '}',
+        '[' : ']'
+    }
+
+    for(let char of s) {
+        if(data[char]) {
+            stack.push(data[char])
+        } else {
+            if(stack.pop() !== char) {
+                return false
+            }
+        }
+    }
+}
+
+/* Problem 16 : Merge Two Sorted Lists
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+
+*/
+
+// answer : 
+
+var mergeTwoLists = function(list1, list2) {
+     
+    let dummy = new ListNode(-1);
+    let current = dummy;
+
+    while(list1 !== null && list2 !== null) {
+        if(list1.val <= list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+    current.next = list1 !== null ? list1 : list2;
+    return dummy.next;
+
+
+};
